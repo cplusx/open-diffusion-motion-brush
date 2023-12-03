e74802a0f2bc644469c26e123c6bfb64b7e25655
cplusx <1760232507@qq.com>
2023-12-03 13:29:28 +0000

    add example

--- gradio_demo.py
+++ gradio_demo.py
@@ -7,6 +7,8 @@ motion_brush = MotionBrush()
 def reset_max_steps_to_replace_if_invalid(max_steps_to_replace, num_inference_steps):
     if max_steps_to_replace > num_inference_steps:
         return num_inference_steps
+    else:
+        return max_steps_to_replace
 
 def animate_image(
     image,
@@ -22,15 +24,18 @@ def animate_image(
     mask = image['layers'][0][..., -1:]
     mask = (mask > 128).astype(np.float32)
 
+    if mask.sum() == 0:
+        mask = None
+
     gif_path = motion_brush(
         source_image, 
         mask,
-        max_steps_to_replace=max_steps_to_replace,
-        num_frames=num_frames,
-        num_inference_steps=num_inference_steps,
-        fps=fps,
-        motion_bucket_id=motion_bucket_id,
-        seed=seed,
+        max_steps_to_replace=int(max_steps_to_replace),
+        num_frames=int(num_frames),
+        num_inference_steps=int(num_inference_steps),
+        fps=int(fps),
+        motion_bucket_id=int(motion_bucket_id),
+        seed=int(seed),
     )
     return gif_path
 
@@ -41,14 +46,21 @@ with gr.Blocks() as demo:
 
     with gr.Row():
         with gr.Column():
-            num_inference_steps = gr.Slider(minimum=5, maximum=50, step=5, default=25, label="Number of Inference Steps")
-            max_steps_to_replace = gr.Slider(minimum=1, maximum=50, step=1, default=25, label="Max Steps to Replace")
-            motion_bucket_id = gr.Slider(minimum=0, maximum=300, step=1, default=100, label="Motion Bucket ID")
-        num_frames = gr.Textbox(label="Number of Frames", default=25)
-        fps = gr.Textbox(label="FPS", default=8)
-        seed = gr.Textbox(label="Seed", default=42)
+            num_inference_steps = gr.Slider(minimum=20, maximum=50, step=5, value=25, label="Number of Inference Steps")
+            max_steps_to_replace = gr.Slider(minimum=1, maximum=50, step=1, value=25, label="Max Steps to Replace")
+            motion_bucket_id = gr.Slider(minimum=0, maximum=300, step=1, value=30, label="Motion Bucket ID")
+        num_frames = gr.Textbox(label="Number of Frames", value=25)
+        fps = gr.Textbox(label="FPS", value=8)
+        seed = gr.Textbox(label="Seed", value=42)
 
-    submit_btn = gr.Button(text="Generate")
+    submit_btn = gr.Button(value="Generate")
+    
+    gr.Examples(
+        [
+            ['figures/test_image_cloud_resized.jpg']
+        ],
+        inputs=[input_image],
+    )
 
     submit_btn.click(
         animate_image, 
@@ -74,3 +86,16 @@ with gr.Blocks() as demo:
             max_steps_to_replace
         ]
     )
+
+    max_steps_to_replace.change(
+        reset_max_steps_to_replace_if_invalid,
+        inputs=[
+            max_steps_to_replace,
+            num_inference_steps
+        ],
+        outputs=[
+            max_steps_to_replace
+        ]
+    )
+
+demo.launch(share=True)
--- image_utils.py
+++ image_utils.py
@@ -1,8 +1,10 @@
+import os
 from PIL import Image
 import numpy as np
 import imageio
 
 def make_gif(frames, filename, fps=8, rescale=0.5):
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
     # resize frames
     if rescale is not None:
         frames = [Image.fromarray(frame) for frame in frames]
